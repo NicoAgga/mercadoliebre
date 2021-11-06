68b27485b2fcb670e46c4bc374b90eb96006e0d7
Nicolas <nicogarcia98nqn321@gmail.com>
2021-11-06 12:44:08 -0300

    Proyecto en proceso

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(3000);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/register.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
